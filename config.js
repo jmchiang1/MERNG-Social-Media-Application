6a712d069bc22ff840a1c3ffa87c7a414212f820
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-01-07 15:33:28 -0500

    created user and post models

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://jmchiang:darklord5@mthree-mongo.ojzwn.mongodb.net/movies?retryWrites=true&w=majority'
+}
---
+++ models/post.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require('mongoose');
+
+const postSchema = new Schema({
+    title: String,
+    description: String,
+    comments: [
+        {
+            body: String,
+            username: String,
+        }
+    ],
+    likes: [
+        {
+            username: String,
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
+})
+
+module.exports = model('Post', postSchema);
