51c4289f9cdb164429680658e0690ed25fd441d6
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-01-08 23:36:52 -0500

    added deletePost mutation

--- graphql/resolvers/postResolvers.js
+++ graphql/resolvers/postResolvers.js
@@ -1,8 +1,11 @@
+const post = require("../../models/post");
 const Post = require("../../models/post");
 const checkAuth = require('../../util/checkAuth')
+const { AuthenticationError } = require('apollo-server');   //apollo specific authentication error 
 
 module.exports = {
   Query: {
+    //GET ALL POST 
     async getPosts() {
       try {
         const posts = await Post.find().sort()
@@ -11,6 +14,7 @@ module.exports = {
         throw new Error(err);
       }
     },
+    // GET SINGLE POST
     async getPost(_, { postId }) {
       try {
         const post = await Post.findById(postId);
@@ -27,6 +31,7 @@ module.exports = {
     },
   },
   Mutation: {
+    // CREATE POST
     async createPost(_, { title, description }, context){
       const user = checkAuth(context);  //make sure user is authenicated and able to post things 
       console.log("USER", user);
@@ -40,6 +45,21 @@ module.exports = {
 
       const post = await newPost.save();
       return post;
+    },
+    //DELETE POST 
+    async deletePost(_, {postId}, context ){
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username){
+          await Post.deleteOne();
+          return "Post has been deleted!"
+        } else {
+          throw new AuthenticationError('This is not your post to delete')
+        }
+      } catch (err){
+        throw new Error (err);
+      }
     }
   }
 };
--- util/checkAuth.js
+++ util/checkAuth.js
@@ -1,28 +1,4 @@
-// const jwt = require('jsonwebtoken');
-// const SECRET_KEY = require('../config')
-// const { AuthenicationError } = require('apollo-server') //apollo specific authentication error 
-
-// //context with have an object containing the authorization header 
-// module.exports = (context) => {
-//     const authHeader = context.req.headers.authorization;
-//     if (authHeader){
-//         //Bearer ...
-//         const token = authHeader.split('Bearer ')[1];   //split token from bear word 
-//         if (token){
-//             try {
-//                 const user = jwt.verify(token, SECRET_KEY); //if token exist, verify it 
-//                 return user;
-//             } catch (err){
-//                 throw new AuthenicationError('Invalid/Expired Token')
-//             }
-//         }
-//         throw new Error ('Authenication Token Error')   //no token detected 
-//     }
-//     throw new Error ('Authenication Header Error')   //no authHeader detected 
-// }
-
-const { AuthenticationError } = require('apollo-server');
-
+const { AuthenticationError } = require('apollo-server');   //apollo specific authentication error 
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../config');
 
@@ -30,18 +6,18 @@ module.exports = (context) => {
   const authHeader = context.req.headers.authorization; //Cannot read properties of undefined "headers"
   console.log("AUTH-HEADER",authHeader);
   if (authHeader) {
-    // Bearer ....
-    const token = authHeader.split('Bearer ')[1];
+    // Bearer + token value
+    const token = authHeader.split('Bearer ')[1];   //split: get only the token value
     if (token) {
       try {
-        const user = jwt.verify(token, SECRET_KEY);
+        const user = jwt.verify(token, SECRET_KEY); //if user exist, verify the token then return user data 
         return user;
       } catch (err) {
-        throw new AuthenticationError('Invalid/Expired token');
+        throw new AuthenticationError('Invalid/Expired token'); //token error
       }
     }
-    throw new Error("Authentication token must be 'Bearer [token]");
+    throw new Error("Authentication token must be 'Bearer [token]");    //no token detected 
   }
-  throw new Error('Authorization header must be provided');
+  throw new Error('Authorization header must be provided'); //no authHeader detected 
 };
 
