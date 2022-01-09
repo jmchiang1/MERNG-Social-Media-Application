3238de41a696b3f1d3047513e54c5a761d40ee78
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-01-08 22:19:52 -0500

    get single post validation

--- graphql/resolvers/postResolvers.js
+++ graphql/resolvers/postResolvers.js
@@ -1,4 +1,4 @@
-const Post = require('../../models/post')
+const Post = require("../../models/post");
 
 module.exports = {
   Query: {
@@ -6,6 +6,20 @@ module.exports = {
       try {
         const posts = await Post.find();
         return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        console.log("POST", post);
+        if (post !== undefined) {
+          return post;
+        } else {
+          console.log('Post not found')
+          throw new Error('Post not found');
+        }
       } catch (err) {
         throw new Error(err);
       }
